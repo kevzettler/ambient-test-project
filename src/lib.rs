ee5cbe9e390cb595b62814bec628d82f2299e50a
Kev Zettler <kevzettler@gmail.com>
2023-03-30 19:21:50 -0700

    working commit before going out partying mech is facing correct way

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::FRAC_PI_2;
+
 use ambient_api::{
     components::core::{
         game_objects::player_camera,
@@ -6,7 +8,7 @@ use ambient_api::{
         },
         player::{player, user_id},
         prefab::prefab_from_url,
-        primitives::quad,
+        primitives::{cube, quad},
         rendering::color,
         transform::{lookat_center, rotation, scale, translation},
     },
@@ -15,7 +17,7 @@ use ambient_api::{
     player::KeyCode,
     prelude::*,
 };
-use components::player_camera_ref;
+use components::{player_camera_ref, player_mesh_ref};
 
 #[main]
 pub async fn main() -> EventResult {
@@ -24,7 +26,7 @@ pub async fn main() -> EventResult {
         .with_merge(make_transformable())
         .with_default(quad())
         .with(scale(), Vec3::ONE * 10.)
-        .with(color(), vec4(0.5, 0.5, 0.5, 1.))
+        .with(color(), vec4(0.5, 1.0, 0.5, 1.))
         .with_default(plane_collider())
         .spawn();
 
@@ -39,23 +41,32 @@ pub async fn main() -> EventResult {
                 .spawn();
 
             // add mecha to player id
+            let player_mesh_id = Entity::new()
+                .with_merge(make_transformable())
+                .with(prefab_from_url(), asset_url("assets/mecha.glb").unwrap())
+                .with(rotation(), Quat::from_rotation_z(-std::f32::consts::FRAC_PI_2))
+                .spawn();
+
             entity::add_components(
                 id,
+                // root entity and character controller
                 Entity::new()
                     .with_merge(make_transformable())
-                    .with_default(visualizing())
-                    .with(prefab_from_url(), asset_url("assets/mecha.glb").unwrap())
+                    .with_default(cube())
                     .with(player_camera_ref(), camera)
+                    .with(player_mesh_ref(), player_mesh_id)
+                    .with(color(), vec4(0.5, 0.0, 1.0, 1.0))
                     .with(character_controller_height(), 2.)
-                    .with(character_controller_radius(), 0.5),
+                    .with(character_controller_radius(), 0.5)
+                    .with_default(visualizing()),
             );
         }
     });
 
-    query((player(), player_camera_ref()))
+    query((player(), player_camera_ref(), player_mesh_ref()))
         .build()
         .each_frame(move |players| {
-            for (player_id, (_, camera_id)) in players {
+            for (player_id, (_, camera_id, player_mesh_id)) in players {
                 let Some((delta, pressed)) = player::get_raw_input_delta(player_id) else { continue; };
 
                 let forward = entity::get_component(player_id, rotation()).unwrap() * Vec3::X;
@@ -77,17 +88,20 @@ pub async fn main() -> EventResult {
                     direction += right;
                 }
 
-                println!("wtf is player direction {}", &direction.length());
                 entity::mutate_component(player_id, rotation(), |q: &mut Quat| {
                     *q *= Quat::from_rotation_z(delta.mouse_position.x * 0.01)
                 });
 
+                entity::mutate_component(player_mesh_id, rotation(), |q: &mut Quat| {
+                    *q *= Quat::from_rotation_z(delta.mouse_position.x * 0.01)
+                });
 
                 entity::mutate_component(player_id, translation(), |t| *t += direction * speed);
+                entity::mutate_component(player_mesh_id, translation(), |t| *t += direction * speed);
 
                if direction.length() != 0.0 { // play walk
                     entity::set_animation_controller(
-                        player_id,
+                        player_mesh_id,
                         AnimationController {
                             actions: &[AnimationAction {
                                 clip_url: &asset_url("assets/mecha.glb/animations/walk_2.anim").unwrap(),
@@ -99,7 +113,7 @@ pub async fn main() -> EventResult {
                     );
                 }else{ // idle
                     entity::set_animation_controller(
-                        player_id,
+                        player_mesh_id,
                         AnimationController {
                             actions: &[AnimationAction {
                                 clip_url: &asset_url("assets/mecha.glb/animations/idle_1.anim").unwrap(),
@@ -112,9 +126,11 @@ pub async fn main() -> EventResult {
                 }
 
 
-                let pos = entity::get_component(player_id, translation()).unwrap();
-                entity::set_component(camera_id, lookat_center(), pos);
-                entity::set_component(camera_id, translation(), pos - forward * 4. + Vec3::Z * 7.);
+                let player_position = entity::get_component(player_id, translation()).unwrap();
+                let mut target = player_position;
+
+                entity::set_component(camera_id, lookat_center(), target);
+                entity::set_component(camera_id, translation(), target - forward * 4. + Vec3::Z * 7.);
             }
         });
 
