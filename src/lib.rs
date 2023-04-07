30bc4511b89b75025bac3a1543ee4e6077dfb978
Kev Zettler <kevzettler@gmail.com>
2023-04-06 22:36:44 -0700

    implement fucked up rendering quaternion arc ball camera

--- src/lib.rs
+++ src/lib.rs
@@ -4,25 +4,27 @@ use ambient_api::{
     components::core::{
         game_objects::player_camera,
         physics::{
-            character_controller_height, character_controller_radius, plane_collider, visualizing,
+            character_controller_height, character_controller_radius, plane_collider, visualizing
         },
         player::{player, user_id},
         prefab::prefab_from_url,
-        primitives::{cube, quad},
+        primitives::{cube, quad },
         rendering::color,
         transform::{lookat_center, rotation, scale, translation, local_to_parent},
         ecs::{children, parent}
     },
-    concepts::{make_perspective_infinite_reverse_camera, make_transformable},
-    entity::{AnimationAction, AnimationController},
+    concepts::{make_perspective_infinite_reverse_camera, make_transformable, make_sphere},
     player::KeyCode,
     prelude::*,
 };
-use components::{player_camera_ref, player_mesh_ref};
+use components::{player_camera_ref, player_mesh_ref, player_target_ref, player_target_rotation, player_target_vec};
+
 
 #[main]
 pub async fn main() -> EventResult {
-    // ground
+    let camera_lookat_height_offset = Vec3::Z * 7.0;
+
+    // ground entity
     Entity::new()
         .with_merge(make_transformable())
         .with_default(quad())
@@ -33,15 +35,6 @@ pub async fn main() -> EventResult {
 
     spawn_query((player(), user_id())).bind(move |players| {
         for (id, (_, user)) in players {
-            let camera = Entity::new()
-                .with_merge(make_perspective_infinite_reverse_camera())
-                .with_default(player_camera())
-                .with(user_id(), user)
-                .with(translation(), Vec3::ONE)
-                .with(rotation(), Quat::IDENTITY)
-                .with(lookat_center(), vec3(0., 0., 0.))
-                .spawn();
-
             // add mecha to player id
             let player_mesh_id = Entity::new()
                 .with_merge(make_transformable())
@@ -51,6 +44,26 @@ pub async fn main() -> EventResult {
                 .with(rotation(), Quat::from_rotation_z(-std::f32::consts::FRAC_PI_2)) // rotate blender mesh to fit world coordinates
                 .spawn();
 
+            let player_target = Vec3::Z * 7. + Vec3::X * 7.;
+            // camera target mesh for debugging
+            let target_mesh = Entity::new()
+                .with_merge(make_sphere())
+                .with(translation(), player_target)
+                .with(color(), vec4(0., 1.0, 1.0, 1.))
+                .spawn();
+
+            //Quat is default rotation from make_transformable
+            let forward = Quat::from_xyzw(0f32, 0f32, 0f32, 1f32) * Vec3::X;
+            let camera_offset = Vec3::new(0f32, 0f32, 0f32) + player_target -(forward * 5.);
+            let camera = Entity::new()
+                .with_merge(make_perspective_infinite_reverse_camera())
+                .with_default(player_camera())
+                .with(user_id(), user)
+                .with(translation(), camera_offset)
+                .with(lookat_center(), player_target)
+                .spawn();
+
+            // create root player entity
             entity::add_components(
                 id,
                 // root entity and character controller
@@ -59,6 +72,9 @@ pub async fn main() -> EventResult {
                     .with_default(cube())
                     .with(children(), vec![player_mesh_id])
                     .with(player_camera_ref(), camera)
+                    .with(player_target_ref(), target_mesh)
+                    .with(player_target_rotation(), Quat::IDENTITY)
+                    .with(player_target_vec(), player_target)
                     .with(player_mesh_ref(), player_mesh_id)
                     .with(color(), vec4(0.5, 0.0, 1.0, 1.0))
                     .with(character_controller_height(), 2.)
@@ -68,10 +84,10 @@ pub async fn main() -> EventResult {
         }
     });
 
-    query((player(), player_camera_ref(), player_mesh_ref()))
+    query((player(), player_camera_ref(), player_mesh_ref(), player_target_ref()))
         .build()
         .each_frame(move |players| {
-            for (player_id, (_, camera_id, player_mesh_id)) in players {
+            for (player_id, (_, camera_id, player_mesh_id, target_id)) in players {
                 let Some((delta, pressed)) = player::get_raw_input_delta(player_id) else { continue; };
 
                 let forward = entity::get_component(player_id, rotation()).unwrap() * Vec3::X;
@@ -95,63 +111,39 @@ pub async fn main() -> EventResult {
 
                 //update player rotation
                 entity::mutate_component(player_id, rotation(), |q: &mut Quat| {
-                    *q *= Quat::from_rotation_z(delta.mouse_position.x * 0.01)
+                    *q *= Quat::from_rotation_z(delta.mouse_position.x * 0.01);
                 });
 
-                // upate player translation
+                // udpate player translation
                 entity::mutate_component(player_id, translation(), |t| *t += player_direction * speed);
+
+                // update camera rotation on vertical access
+                entity::mutate_component(player_id, player_target_rotation(), |q: &mut Quat| {
+                   *q *= Quat::from_rotation_y(delta.mouse_position.y * 0.01);
+                });
+                let player_rotation = entity::get_component(player_id, rotation()).unwrap();
+                let view_vert_rotation = entity::get_component(player_id, player_target_rotation()).unwrap();
+                let camera_rotation_quat = player_rotation * view_vert_rotation;
+
 
 
                 let player_position = entity::get_component(player_id, translation()).unwrap();
-                let camera_player_distance: Vec3 = Vec3::NEG_ONE * forward * 5.;
-                let camera_height_offset: Vec3 = Vec3::Z * 9.5;
-                let camera_position: Vec3 = player_position + camera_player_distance + camera_height_offset;
-                entity::set_component(camera_id, translation(), camera_position);
-//                entity::set_component(camera_id, lookat_center(), player_position);
 
-                // update camera vertical rotation
-                entity::mutate_component(camera_id, rotation(), |q: &mut Quat|{
-                    *q *= Quat::from_rotation_z(delta.mouse_position.x * 0.01);
-                    *q *= Quat::from_rotation_y(delta.mouse_position.y * 0.01);
-                });
-
-                let camera_rotation = entity::get_component(camera_id, rotation()).unwrap();
-                let camera_target_height_offset: Vec3 = Vec3::Z * 7.5;
-                // WTF is this correct? tried to copy camera 'front' from the typescript codebase
-                let camera_forward = camera_rotation.mul_vec3(Vec3::X);
-                let camera_target_distance: f32 = 150.;
-                let camera_target = player_position + camera_target_height_offset + ( camera_forward * camera_target_distance);
-
-                entity::set_component(camera_id, lookat_center(), camera_target);
+                let camera_front = camera_rotation_quat * Vec3::X;
+                let target_projection = camera_front * 30.;
+                let camera_height_offset = Vec3::Z * 7.;
+                let target_position = player_position + camera_height_offset + target_projection;
 
 
-               // Animation controllers
-               if player_direction.length() != 0.0 { // play walk
-                    entity::set_animation_controller(
-                        player_mesh_id,
-                        AnimationController {
-                            actions: &[AnimationAction {
-                                clip_url: &asset_url("assets/mecha.glb/animations/walk_2.anim").unwrap(),
-                                looping: true,
-                                weight: 1.,
-                            }],
-                            apply_base_pose: false,
-                        },
-                    );
-                }else{ // idle
-                    entity::set_animation_controller(
-                        player_mesh_id,
-                        AnimationController {
-                            actions: &[AnimationAction {
-                                clip_url: &asset_url("assets/mecha.glb/animations/idle_1.anim").unwrap(),
-                                looping: true,
-                                weight: 1.,
-                            }],
-                            apply_base_pose: false,
-                        },
-                    );
-                }
+                // move the target debug mesh to new target location
+                entity::set_component(target_id, translation(), target_position);
 
+                // update camera lookat
+                entity::set_component(camera_id, lookat_center(), target_position);
+
+                let camera_projection = camera_front * Vec3::NEG_ONE * 10.;
+                let eye_position: Vec3 = player_position + camera_height_offset + camera_projection;
+                entity::set_component(camera_id, translation(), eye_position);
             }
         });
 
